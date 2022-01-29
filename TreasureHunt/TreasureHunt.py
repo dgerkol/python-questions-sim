bf26a3a912a11e036cd6f4b6ca5142dc45695701
Dan Gerkol <d.gerkol@gmail.com>
2022-01-29 23:49:17 +0200

    added map explore functionality

--- TreasureHunt/TreasureHunt.py
+++ TreasureHunt/TreasureHunt.py
@@ -1,9 +1,7 @@
 import random
-from shutil import move
 
 
 MAP_DIR = './Treasure Map'
-
 
 def genMap(mapFile):
     with open(mapFile, 'w+') as treasureMap:
@@ -30,9 +28,32 @@ def mapStats(mapFile):
 
 def exploreMap(mapFile):
     with open(mapFile) as treasureMap:
-        move = playerTurn()
-        treasureMap.seek(treasureMap.tell()+move)
+        pos = 0
+        eof = treasureMap.seek(0,2)
         
+        while True:
+            move, leap = playerTurn()
+            
+            if move == '1':
+                if (pos + leap) > eof:
+                    treasureMap.seek((pos + leap) - eof - 1 )
+                    pos = treasureMap.tell()
+                else:
+                    treasureMap.seek(pos + leap)
+                    pos = treasureMap.tell()
+            else:
+                if (pos - leap) < 0:
+                    #Not using seek(value!=0,2) since python3 does not support nonzero end-relative seeks(!)
+                    treasureMap.seek(eof - (leap - pos) - 1)
+            
+            hit = treasureMap.readline(1)
+            pos = treasureMap.tell()-1
+            if hit in ['TREASURE']:
+                return True
+            else:
+                print(f'You hit {hit}')
+            treasureMap.seek(pos)
+            
 
 
 def playerTurn():
@@ -40,12 +61,24 @@ def playerTurn():
         move = input("sel 1,2: ")
         if move not in ['1', '2']:
             continue
-        else:
-            return move
+        
+        leap = input("leap: ")
+        try:
+            leap = int(leap)
+        except:
+            print("Not a number")
+            continue
+        
+        return move, int(input("leap: "))
 
 
 
 # --- Game Functionality Begin --- #
+movesCount = 1
 genMap(MAP_DIR)
-mapData = exploreMap(MAP_DIR)
+if exploreMap(MAP_DIR):
+    print("you win!")
+    print(f'Total moves: {movesCount}')
+else:
+    movesCount += 1
 
